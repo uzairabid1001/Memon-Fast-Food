f2bbeefbd6d81b8c0864dfbe57ae71e92c84d4fe
uzairabid1001 <uzairabid1001@gmail.com>
2025-05-04 19:06:27 +0500

    Memon

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const locationSelect = document.getElementById('location');
 const popup = document.getElementById('popup');
 
 
-// deliveryBtn.addEventListener('click', () => {
-//   deliveryBtn.classList.add('active');
+deliveryBtn.addEventListener('click', () => {
+  deliveryBtn.classList.add('active');
   
-// });
+});
 
 
 
-// // Handle select button click
-// selectBtn.addEventListener('click', () => {
-//   const location = locationSelect.value;
-//   if (!location) {
-//     alert('Please select a location!');
-//     return;
-//   }
-//   popup.innerHTML=""
-//   // Close popup or proceed
-//   // alert(`Order Type: ${deliveryBtn.classList.contains('active') ? 'Delivery' : 'Pickup'}\nLocation: ${location}`);
+// Handle select button click
+selectBtn.addEventListener('click', () => {
+  const location = locationSelect.value;
+  if (!location) {
+    alert('Please select a location!');
+    return;
+  }
+  popup.innerHTML=""
+  // Close popup or proceed
+  // alert(`Order Type: ${deliveryBtn.classList.contains('active') ? 'Delivery' : 'Pickup'}\nLocation: ${location}`);
   
-//   document.getElementById('popup').style.display = 'none';
-// });
+  document.getElementById('popup').style.display = 'none';
+});
